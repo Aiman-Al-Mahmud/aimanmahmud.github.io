69ee519ea5c0a03cbcdfa322ce868a0a499a95e8
Aiman-Al-Mahmud <aimanmahmud69@gmail.com>
2025-09-20 20:24:26 +0600

    fixed for vercel4

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+return tap(Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -15,4 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })->create(), function (Application $app) {
+    if (isset($_ENV['VERCEL'])) {
+        $app->useStoragePath('/tmp/storage');
+    }
+});
